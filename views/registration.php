3b9d5249abce7839062fa65dca906e030bb0fd7d
b4rsmets <slavunq8@gmail.com>
2023-02-22 22:05:53 +0700

    Патерны для формы

--- views/registration.php
+++ views/registration.php
@@ -22,13 +22,13 @@ else{
             <label for="email"><b>Email</b></label>
             <input type="email"  placeholder="Email" name="email" required>
             <label for="password"><b>Пароль</b></label>
-            <input type="password"  placeholder="Введите пароль (минимум 6 символов)" name="password" required>
+            <input type="password" pattern=".{6,}"  placeholder="Введите пароль (минимум 6 символов)" name="password" required>
             <hr class="line-reg">
             <p>Создавая аккаунт Вы соглашаетесь с <a href="#">Terms & Privacy</a>.</p>
             <button type="submit" id="reg-btn" class="registerbtn">Зарегистрироваться</button>
         </div>
         <div id="error" class="error">
-            <h2>Такой логин уже существует или пароль меньше 6 символов!</h2>
+            <h2>Такой логин уже существует!</h2>
         </div>
     </form>
 </div>
